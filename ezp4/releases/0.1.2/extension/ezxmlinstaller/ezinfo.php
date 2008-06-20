48877ca5402bd4235461859aba7c2c1a3ec435fe
dis <info@example.com>
2008-06-20 12:31:10 +0000

    - Created release 0.1.2.
    git-svn-id: http://svn.projects.ez.no/ezxmlinstaller@12 de1ecc26-1c65-46f3-84d8-c63194b32b8d

---
+++ ezp4/releases/0.1.2/extension/ezxmlinstaller/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class ezxmlinstallerInfo
+{
+    function info()
+    {
+        return array( 'Name' => "eZ Publish XML Installer",
+                      'Version' => "0.1.2",
+                      'Copyright' => "Copyright (C) 2008 dis@ez.no",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+?>
